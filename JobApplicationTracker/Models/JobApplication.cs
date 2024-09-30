43479843ee8cc8a41895c802e9ee564e52c36bdf
Atanas <atodorov98@gmail.com>
2024-09-30 14:57:27 +0300

    adds models

---
+++ JobApplicationTracker/Models/JobApplication.cs
@@ -0,0 +1,12 @@
+﻿namespace JobApplicationTracker.Models
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Position { get; set; }
+        public DateTime AppliedDate { get; set; }
+        public JobApplicationStatus JobApplicationStatus { get; set; }
+        public string Notes { get; set; }
+    }
+}
